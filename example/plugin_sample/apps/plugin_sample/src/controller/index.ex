e558448061638fa6e7e92a1a79cc242adc5e863e
glejeune <gregoire.lejeune@free.fr>
2014-07-28 19:47:18 +0200

    Use new renderer

--- example/plugin_sample/apps/plugin_sample/src/controller/index.ex
+++ example/plugin_sample/apps/plugin_sample/src/controller/index.ex
@@ -1,21 +1,21 @@
 defmodule Index do
   def get(_request) do
-    :paris_response.render_view(:ok)
+    :paris_response.render(:html, [template: :ok])
   end
 
   def post(_request) do
-    :paris_response.render_view(:ok)
+    :paris_response.render(:html, [template: :ok])
   end
   
   def put(_request) do
-    :paris_response.render_view(:ok)
+    :paris_response.render(:html, [template: :ok])
   end
   
   def head(_request) do
-    :paris_response.render_view(:ok)
+    :paris_response.render(:html, [template: :ok])
   end
   
   def delete(_request) do
-    :paris_response.render_view(:ok)
+    :paris_response.render(:html, [template: :ok])
   end
 end
